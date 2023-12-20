5f092ee75151b3bf5d8295ad670ed3fb475cd276
ksburhan <burhan.koeseler@hotmail.de>
2023-12-20 22:54:46 +0900

    client: create post component and send request

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
-    return <div>Garbanzo App</div>;
+    return <div className='container'>
+        <h1>Create Post</h1>
+        <PostCreate/>
+    </div>;
 };
 
 export default App;
