40a50559ed45c14b86ab8b49f9fc7f5b9bc632be
ksburhan <burhan.koeseler@hotmail.de>
2023-12-23 22:04:12 +0900

    client: render comment list

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const CommentList = ({postId}) => {
-    const [comments, setComments] = useState('');
+    const [comments, setComments] = useState([]);
 
     const fetchData = async () => {
         const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
